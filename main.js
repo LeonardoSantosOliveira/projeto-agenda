1b640651e440220f333f56ba54aff025eff923ac
LeonardoSantosOliveira <leoleosantos21@gmail.com>
2023-06-15 21:37:08 -0300

    Bug fix

--- main.js
+++ main.js
@@ -12,8 +12,10 @@ function cadastrar(){
     var nome = document.getElementById('nome-contato')
     var numero = document.getElementById('numero-contato')
     
-    if (arrayNomes.includes(nome.value) || arrayNumeros.includes(numero.value)){
-        alert('O contato já foi adicionado') 
+    if (arrayNomes.includes(nome.value)){
+        alert('Nome de contato já adicionado, por favor, escolha outro!') 
+    } else if (arrayNumeros.includes(numero.value)){
+        alert('Número já cadastrado. Por favor, Cadastre outro!')
     } else {
         arrayNomes.push(nome.value)
         arrayNumeros.push(numero.value)
